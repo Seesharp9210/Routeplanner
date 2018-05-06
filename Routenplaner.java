6a81cd64f3b295f8d74d81f9254584923e4ef39a
Seesharp9210 <39015479+Seesharp9210@users.noreply.github.com>
2018-05-06 02:18:02 +0200

    Add files via upload

---
+++ Routenplaner.java
@@ -0,0 +1,40 @@
+package de.uniStuttgart.routenplaner;
+
+import java.io.*;
+
+public class Routenplaner {
+
+	private int nodes [];
+	private int edges [];
+	private int latitude [];
+	private int longitude [];
+	private int weights[];
+	private int source [];
+	private int target [];
+	
+	
+	
+	public Routenplaner(String mapAdress){
+		
+	}
+	
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+	}
+	
+	
+	public static void readMap(String s)throws FileNotFoundException{
+		FileReader fr = new FileReader(s);
+		BufferedReader br = new BufferedReader(fr);
+		
+		
+	}
+
+	
+	
+	
+}
